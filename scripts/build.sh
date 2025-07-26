6f651a4d37c7a4a7e22145da8136a27940106d61
Julio Castillo <julio.a.castillo.c@gmail.com>
2025-07-25 18:16:32 -0700

    ci: update build script to include installation directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,13 +4,17 @@ set -e  # Exit on error
 
 # Set build directory
 BUILD_DIR="build"
+INSTALL_DIR="install"
 
 # Create and enter build directory
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
 
 # Run CMake
-cmake ..
+cmake -DCMAKE_INSTALL_PREFIX="../$INSTALL_DIR" ..
 
 # Build all targets with all cores
 make -j"$(nproc)"
+
+# Install to ../install
+make install
