432bada5e0fc0da20235ac86f6dc7c92eebff67e
Julio Castillo <julio.a.castillo.c@gmail.com>
2025-07-26 01:08:18 -0700

    fix: relative paths issues

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -4,10 +4,7 @@
 #include "example_static.hpp"
 #include "example_usage.hpp"
 #include "plugin_api.hpp"
-#include <filesystem>
-#include <fstream>
 #include <iostream>
-#include <vector>
 
 int main(int argc, char** argv)
 {
@@ -18,35 +15,12 @@ int main(int argc, char** argv)
     example_interface();
     example_usage();
 
-    // Determine executable directory
-    std::filesystem::path exe_path = std::filesystem::absolute(argv[0]);
-    std::filesystem::path exe_dir = exe_path.parent_path();
-
-    // List of plugin paths to try (relative to executable location)
-    std::vector<std::filesystem::path> plugin_paths = {
-        exe_dir / "../lib/libexample_plugin_impl.so",  // Installed layout
-        exe_dir / "libexample_plugin_impl.so",         // Same dir as exe (rare, but possible)
-        exe_dir / "../../src/example_plugin_impl/libexample_plugin_impl.so",       // Dev layout
-        exe_dir / "../../build/src/example_plugin_impl/libexample_plugin_impl.so"  // Alt dev
-    };
-
-    bool plugin_loaded = false;
-    for (const auto& path : plugin_paths)
-    {
-        if (std::filesystem::exists(path))
-        {
-            std::cout << "Loading plugin from: " << path << std::endl;
-            load_plugin(path.c_str());
-            plugin_loaded = true;
-            break;
-        }
-    }
-
-    if (!plugin_loaded)
-    {
-        std::cerr << "ERROR: Plugin could not be loaded from any expected location!" << std::endl;
-        return 1;
-    }
+    // Plugin loading with RPATH-based discovery
+    // dlopen() uses RPATH to find plugin automatically - no hardcoded paths!
+    const std::string plugin_name = "libexample_plugin_impl.so";
+    
+    std::cout << "Loading plugin: " << plugin_name << std::endl;
+    load_plugin(plugin_name.c_str());
 
     std::cout << "Main application finished.\n";
     return 0;
